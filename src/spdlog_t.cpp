8369355b71da096b42ce107611e59d8981b6c7d8
rusami <isehara.jp@coderdojo.com>
2022-11-06 17:54:36 +0900

    # to add some operating mode

--- src/spdlog_t.cpp
+++ src/spdlog_t.cpp
@@ -1,4 +1,5 @@
 #include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
 
 int main()
 {
@@ -12,5 +13,14 @@ int main()
     int number_int = 10;
     float number_float = 12.345;
     spdlog::info("number_int is {:03d}", number_int);
+
+    // set log level which also affects to the file log
+    spdlog::set_level(spdlog::level::warn);
     spdlog::info("number_float is {:03.2f}", number_float);
+
+    // logging to the log file
+    auto log_file = spdlog::basic_logger_mt("logger", "log/basic.txt");
+    log_file -> info("hello!");
+    spdlog::set_default_logger(log_file);
+    spdlog::warn("warning! write to the log file");
 }
